a575f38adeee63a70b3921bcefb8c8a47b88345d
Kyrylo Stepanenko <Kyrylo Stepanenko>
2016-04-15 00:30:12 +0300

    Добавлен класс Point

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,6 +10,21 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            /*int x1 = 1;
+            int y1 = 2;
+            string sym1 = "q";
+            int x2 = 10;
+            int y2 = 11;
+            string sym2 = "t";
+            */
+            Point p1 = new Point();
+            p1.x = 6;
+            p1.y = 2;
+            p1.symbol = "y";
+
+            p1.Draw();
+            Console.ReadLine();
         }
+        
     }
 }
